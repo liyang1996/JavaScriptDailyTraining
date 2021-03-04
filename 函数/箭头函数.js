79daed8d41f2e61c063d7771732876c1abaf0a35
yang.aa.li <yang.aa.li@icloud.com>
2021-03-05 00:26:37 +0800

    Revert "0305push"
    This reverts commit e82d65593e40216c8b764e4e0076c938a5cf9801.

--- 函数/箭头函数.js
+++
@@ -1,6 +0,0 @@
-// let hd=[1,2,3,45,5].filter(value => {
-//     return value<=3;
-// })
-let hd=[1,2,3,45,5].reduce((a,b)=>a+b);
-//递归函数使用时他没有名字，回调不方便，构造函数，时间处理器的时候不方便使用，要考虑作用域，考虑到this
-console.log(hd);
